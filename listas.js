a29ab4513deadc3bfad53dda0ea12968fe3be91b
Conceição Peres <marcos.quintanilha@midway.com.br>
2022-04-06 14:12:27 -0300

    trabalhando com listas

---
+++ listas.js
@@ -0,0 +1,10 @@
+const lista = new Array('Salvador', 'São Paulo', 'Rio de Janeiro')
+console.log(lista)
+
+lista.push('Curitiba', 'Florianópolis')
+console.log(lista)
+
+lista.splice(1,1)
+console.log(lista)
+
+console.log(lista[1])
--- primriroPrograma.js
+++ primriroPrograma.js
@@ -1,1 +1,4 @@
-console.log("Meu primeiro progtama")
+console.log("Meu primeiro prograama: trablhando com variáveis")
+const nome = "Conceição"
+const idade = 45
+console.log(`Nome: ${nome} Idade: ${idade}.`)
